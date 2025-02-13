8db2bed650d964d2d9a63b2d9de9617d5ff6f9c4
omar albarakeh <omaralbarakeh2@gmail.com>
2025-02-13 16:59:33 +0200

    updated

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -11,17 +11,24 @@ class _splashscreenState extends State<splashscreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: double.infinity,
-        height: double.infinity,
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-              begin:Alignment.center,
-              end: Alignment.bottomCenter,
-            colors: [Color(0xFF192A3C), Colors.purple],
-          ),
+        body: Container(
+      width: double.infinity,
+      height: double.infinity,
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.center,
+          end: Alignment.bottomCenter,
+          colors: [Color(0xFF192A3C), Colors.purple],
+        ),
       ),
-      )
-    );
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text("Weather",style: TextStyle(color: Colors.yellow,fontSize: 80,fontWeight: FontWeight.bold),),
+              Text("Forecasts",style: TextStyle(color: Colors.white,fontSize: 80,fontWeight: FontWeight.bold))
+            ],
+          ),
+    ));
   }
 }
