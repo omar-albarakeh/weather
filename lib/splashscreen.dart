2c5bfc653c0ddcd1830897a4bc2df11c2241018a
omar albarakeh <omaralbarakeh2@gmail.com>
2025-02-14 15:46:30 +0200

    updated

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:weatherapp/HomePage.dart';
+import 'MyHomePage.dart';
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -10,7 +10,6 @@ class SplashScreen extends StatefulWidget {
 
 class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
   late final AnimationController _controller;
-  late final Animation<double> _cloudMovement;
   late final Animation<double> _rainDropFall;
   late final Animation<double> _sunFadeIn;
 
@@ -23,9 +22,7 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
       duration: const Duration(seconds: 3),
     )..repeat(reverse: true);
 
-    _cloudMovement = Tween<double>(begin: -40, end: 40)
-        .animate(CurvedAnimation(parent: _controller, curve: Curves.linear));
-    _rainDropFall = Tween<double>(begin: 0, end: 40)
+    _rainDropFall = Tween<double>(begin: 0, end: 100)
         .animate(CurvedAnimation(parent: _controller, curve: Curves.linear));
     _sunFadeIn = CurvedAnimation(parent: _controller, curve: Curves.easeIn);
   }
@@ -56,47 +53,39 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             SizedBox(
-              height: 250,
+              height: 300,
               child: Stack(
                 alignment: Alignment.center,
                 children: [
                   Positioned(
                     top: 40,
-                    right: screenWidth * 0.6,
+                    right: screenWidth * 0.45,
                     child: FadeTransition(
                       opacity: _sunFadeIn,
-                      child: const Icon(Icons.wb_sunny, size: 120, color: Colors.amber),
+                      child: const Icon(Icons.wb_sunny, size: 150, color: Colors.amber),
                     ),
                   ),
-                  for (double i = -40; i <= 40; i += 20)
+                  for (double i = 0; i <= 80; i += 20)
+                    for (double j = 0; j <= 80; j += 20)
                     AnimatedBuilder(
                       animation: _rainDropFall,
                       builder: (context, child) {
                         return Positioned(
-                          top: 120 + _rainDropFall.value,
+                          top: 150 + _rainDropFall.value,
                           left: screenWidth / 2.3 + i,
-                          child: const Icon(Icons.water_drop, size:25, color: Colors.blue),
+                          child: const Icon(Icons.water_drop, size: 25, color: Colors.blue),
                         );
                       },
                     ),
                   Positioned(
                     top: 50,
                     left: screenWidth * 0.3,
-                    child: AnimatedBuilder(
-                      animation: _cloudMovement,
-                      builder: (context, child) {
-                        return Transform.translate(
-                          offset: Offset(_cloudMovement.value, 0),
-                          child: const Icon(Icons.cloud, size: 140, color: Colors.white),
-                        );
-                      },
-                    ),
+                    child: const Icon(Icons.cloud, size: 200, color: Colors.white),
                   ),
                 ],
               ),
             ),
             const SizedBox(height: 20),
-            // Static Text Widgets
             const Text(
               "Weather",
               style: TextStyle(
@@ -116,7 +105,6 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
               ),
             ),
             const SizedBox(height: 20),
-            // Get Started Button
             ElevatedButton(
               style: ElevatedButton.styleFrom(
                 backgroundColor: const Color(0xFFFFC905),
@@ -125,7 +113,7 @@ class _SplashScreenState extends State<SplashScreen> with SingleTickerProviderSt
               onPressed: () {
                 Navigator.push(
                   context,
-                  MaterialPageRoute(builder: (context) => const Homepage()),
+                  MaterialPageRoute(builder: (context) => const Myhomepage()),
                 );
               },
               child: const Text(
